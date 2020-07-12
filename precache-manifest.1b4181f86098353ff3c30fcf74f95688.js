990adff743646dd014d68546feca0d37567dec15
Christina Lupanow <celupanow@gmail.com>
2020-07-11 23:47:11 -0600

    Updates

---
+++ precache-manifest.1b4181f86098353ff3c30fcf74f95688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4bdf7ce0ca1c91c0733fa00b406cdc",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "ddf45182ef21a33d09b6",
+    "url": "/myapp/static/css/main.218085df.chunk.css"
+  },
+  {
+    "revision": "4904a472167c683d7147",
+    "url": "/myapp/static/js/2.462b66f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.462b66f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf45182ef21a33d09b6",
+    "url": "/myapp/static/js/main.737a0122.chunk.js"
+  },
+  {
+    "revision": "dfc084a7ee4f2eebf7cd",
+    "url": "/myapp/static/js/runtime-main.5f0af923.js"
+  },
+  {
+    "revision": "77323ef4d056ff730e58f169a048c2a1",
+    "url": "/myapp/static/media/onering.77323ef4.jpg"
+  }
+]);
