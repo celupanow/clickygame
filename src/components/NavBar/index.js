8f86a9357b7b80c2ff2166db1a6aaa25463cb4e4
Lady Clu <celupanow@gmail.com>
2020-10-20 17:12:42 -0600

    adding readme image

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,9 +6,9 @@ import "./style.css";
 
 const NavBar = props =>  (
     <nav className="navbar">
-      <div class="title">Lord of the Clicks</div>
-      <div class="message">{props.message}<p>Try Not To Click The Same Character Twice!</p></div>
-      <div class="score">Score: {props.score}</div>
+      <div className="title">Lord of the Clicks</div>
+      <div className="message">{props.message}<p>Try Not To Click The Same Character Twice!</p></div>
+      <div className="score">Score: {props.score}</div>
       </nav>
   );
 
